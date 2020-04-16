b53832f9f62bc7def4f095c1b4824a751156ea29
kaifan-dash <kaifan@dashmote.com>
2020-04-16 15:56:39 +0800

    dist wheel

--- build/lib/helper/DataHelper.py
+++ build/lib/helper/DataHelper.py
@@ -1,8 +1,25 @@
 import pandas as pd 
+from datetime import date, timedelta, datetime
+import os
+
+def get_dates():
+    dates = []
+    for i in range(1, datetime.now().day+1):
+        day = date(datetime.now().year, datetime.now().month, i)
+        day = day.strftime('20%y%m%d')
+        dates.append(day)
+    return dates
 
 class DataHelper():
     def __init__(self, manager=''):
         self.manager = manager
+        try:
+            with open('/home/ubuntu/notebook/.secret','r') as f:
+                account=f.readlines()
+            os.environ['AWS_ACCESS_KEY_ID']=account[0].strip()
+            os.environ['AWS_SECRET_ACCESS_KEY']=account[1].strip()
+        except:
+            print ('could not locate notebook AWS credentials')
 
     def read_parquet(self, bucket, platform, date, country, option):
         if option == 'outlet' or option == 'information':
@@ -30,4 +47,21 @@ class DataHelper():
         meal_df = meal_df[meal_df['id_source'] == id_source]
         return meal_df[meal_df['name'].str.contains(meal_name)]
 
+    def concat_daily(self, file_list, bucket='dashmote-product-daily', dup_filter = ['id_source']):
+        df = pd.DataFrame()
+        for key in file_list:
+            print (key)
+            _file = pd.read_parquet(f's3://{bucket}/{key}')
+            df = pd.concat([_file, df], sort=False)
+            del(_file)
+        df = df.drop_duplicates(dup_filter).reset_index()
+        return df
 
+    def get_files_by_dates(self, bucket, platform, country, option):
+        if self.manager == '':
+            return 's3 manager not setup correctly'
+        results = []
+        for _date in get_dates():
+            files = self.manager.get_files(bucket, f'{platform}/{_date}/{country}_outlet_{option}')
+            results.extend(files)
+        return results
